03608fbdfa74386bfd8858d64c9db90c77f7b71e
asejdina <asejdina@gmail.com>
2015-07-16 00:40:32 -0400

    updated versions of apps

---
+++ stopbullying/incidents.js
@@ -0,0 +1,57 @@
+Parse.$ = jQuery;
+
+Parse.initialize('A1HlJySQ3gfopy6J8YC6sghYM5yihOkCpXOrmbXb',
+'4hyL5OUH4AKeQ5hus6xVH84FDirM2r0bI1JAQzKR');
+
+$(function() {
+
+  $(document).ready(init);
+    // Ensure that each todo created has `content`.
+  function init() {
+    var Incident = Parse.Object.extend("Incident");
+    var query = new Parse.Query(Incident);
+    //query.equalTo("playerName", "Dan Stemkoski");
+    query.find({
+      success: function(results) {
+        //alert("Successfully retrieved " + results.length + " scores.");
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          var html =  '<div class="oneresult">' +
+                      '<h1>issue type</h1>' +
+                      object.get('issuetype') +
+                      '<h1>location</h1>' +
+                      object.get('location') +
+                      '<h1>issue</h1>' +
+                      object.get('issue') +
+                      '</div>';
+          $("#results").append(html);
+        }
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+    //$('#submit').on('click', closeIncident);
+  }
+});
+
+//   function closeIncident() {
+//   ALL OF THIS NEEDS TO CHANGE TO CLOSE AN INCIDENT.
+//     var incident = new Parse.Object('Incident');
+//     var issue = $('#issue').val();
+//     var location = $('#location').val();
+//     var issuetype = $('input:radio:checked').attr('id');
+//     incident.set('issue', issue);
+//     incident.set('location', location);
+//     incident.set('issuetype', issuetype);
+//     incident.save(null, {
+//       success: function(incident){
+//         window.location.replace('home.html');
+//       },
+//       error: function(tag, error){
+//         alert('Sorry, there was an error saving your incident.');
+//       }
+//     });
+//   }
+// });
---
+++ stopbullying/main.js
@@ -0,0 +1,31 @@
+Parse.$ = jQuery;
+
+Parse.initialize('A1HlJySQ3gfopy6J8YC6sghYM5yihOkCpXOrmbXb',
+'4hyL5OUH4AKeQ5hus6xVH84FDirM2r0bI1JAQzKR');
+
+$(function() {
+
+  $(document).ready(init);
+  // Ensure that each todo created has `content`.
+  function init() {
+    $('#submit').on('click', newIncident);
+  }
+
+  function newIncident() {
+    var incident = new Parse.Object('Incident');
+    var issue = $('#issue').val();
+    var location = $('#location').val();
+    var issuetype = $('input:radio:checked').attr('id');
+    incident.set('issue', issue);
+    incident.set('location', location);
+    incident.set('issuetype', issuetype);
+    incident.save(null, {
+      success: function(incident){
+        window.location.replace('home.html');
+      },
+      error: function(tag, error){
+        alert('Sorry, there was an error saving your incident.');
+      }
+    });
+  }
+});
