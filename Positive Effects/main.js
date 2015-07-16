0744429663f8eef5baf9cd7bcbe72d2cc2518188
asejdina <asejdina@gmail.com>
2015-07-16 06:34:33 -0400

    added all four projects

--- Positive Effects/main.js
+++ Positive Effects/main.js
@@ -8,12 +8,19 @@ $(function() {
   var map;
   var array = [];
 
+
+
   $(document).ready(init);
   // Ensure that each todo created has `content`.
   function init() {
+
+    $('.newTag').on('click', newTag);
+    $('.closeModal').on('click', closeModal);
     $('#submitPhoto').on('click', photoUpload);
+
     var tag = Parse.Object.extend('Tag');
     var query = new Parse.Query(tag);
+
     query.find({
       success: function(results){
         for (var i = 0; i < results.length; i++) {
@@ -21,25 +28,74 @@ $(function() {
           var location = object.get('location').toJSON();
           var latitude = location.latitude;
           var longitude = location.longitude;
-          createMarker(latitude, longitude);
+          var id = object.id;
+          createMarker(latitude, longitude, id);
         }
       },
-      error: function(error) { alert("Error: " + error.code + " " + error.message); }
+      error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
     });
     initMap(41.67, -86.25, 12);
   }
 
+
+
+// =========FORM MODAL
+
+  function newTag(){
+    $('#formModal').css('display', 'block');
+  }
+
+  function closeModal(){
+    $('#formModal').css('display', 'none');
+    $('#artModal').css('display', 'none');
+  }
+
+// =============Map
 
   function initMap(lat, lng, zoom){
     var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
-  function createMarker(lat, lng){
+  function createMarker(lat, lng, id){
     var latLng = new google.maps.LatLng(lat,lng);
-    console.log(latLng);
-    new google.maps.Marker({map: map, position: latLng});
+    var marker = new google.maps.Marker({map: map, position: latLng, id: id});
+
+    google.maps.event.addListener(marker, 'click', function() {
+        var tag = Parse.Object.extend('Tag');
+        var query = new Parse.Query(tag);
+        query.equalTo("objectId", this.id)
+        query.find({
+          success: function(results){
+            for (var i = 0; i < results.length; i++) {
+              var object = results[i];
+              var photoURL = object.get("photo").url();
+              var html =  '<div class="artWrapper">' +
+                          '<div class="artInfo">' +
+                          '<h2>Area:</h2>' +
+                          object.get('area') +
+                          '<h2>Time Registered:</h2>' +
+                          object.get('createdAt') +
+                          '<h2>Details:</h2>' +
+                          object.get('comments') +
+                          '</div>' +
+                          '<div class="image" style="background-image: url('+photoURL+')""></div>' +
+                          '</div>';
+              $('#artModal').append(html);
+            }
+          },
+          error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
+        });
+
+        $('#artModal').css('display', 'block');
+      });
   }
+
+  // =========ART MODAL
+
+
+
+//=============Form Submission
 
 
   function photoUpload(){
@@ -73,7 +129,7 @@ $(function() {
                 });
               });
           } else {
-              alert("Geolocation is not supported by this browser.");
+              alert('Geolocation is not supported by this browser.');
           }
 
         }, function(error) {
