b85cf0aacbf18e652cd7377ab8429631d535f07d
asejdina <asejdina@gmail.com>
2015-07-21 07:56:40 -0400

    Got map working on destinations

--- After School/main.js
+++ After School/main.js
@@ -11,6 +11,31 @@ $(function() {
   $(document).ready(init);
 
   function init() {
+    var destination = Parse.Object.extend('Destination');
+    var query = new Parse.Query(destination);
+
+    query.find({
+      success: function(results){
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          var id = object.id;
+          var address = object.get('address');
+
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              var latitude = results[0].geometry.location.lat();
+              var longitude = results[0].geometry.location.lng();
+              createMarker(latitude, longitude, id);
+            } else {
+              alert("Request failed.")
+            }
+          });
+        }
+      },
+      error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
+    });
+
 
     initMap(41.67, -86.25, 12);
   }
@@ -28,34 +53,34 @@ $(function() {
     var latLng = new google.maps.LatLng(lat,lng);
     var marker = new google.maps.Marker({map: map, position: latLng, id: id});
 
-    // google.maps.event.addListener(marker, 'click', function() {
-    //     var tag = Parse.Object.extend('Destination');
-    //     var query = new Parse.Query(tag);
-    //     query.equalTo("objectId", this.id)
-    //     query.find({
-    //       success: function(results){
-    //         for (var i = 0; i < results.length; i++) {
-    //           var object = results[i];
-    //           var photoURL = object.get("photo").url();
-    //           var html =  '<div class="artWrapper">' +
-    //                       '<div class="artInfo">' +
-    //                       '<h2>Area:</h2>' +
-    //                       object.get('area') +
-    //                       '<h2>Time Registered:</h2>' +
-    //                       object.get('createdAt') +
-    //                       '<h2>Details:</h2>' +
-    //                       object.get('comments') +
-    //                       '</div>' +
-    //                       '<div class="image" style="background-image: url('+photoURL+')""></div>' +
-    //                       '</div>';
-    //           $('#artModal').append(html);
-    //         }
-    //       },
-    //       error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
-    //     });
-    //
-    //     $('#artModal').css('display', 'block');
-    //   });
+    google.maps.event.addListener(marker, 'click', function() {
+        var destination = Parse.Object.extend('Destination');
+        var query = new Parse.Query(destination);
+        query.equalTo("objectId", this.id)
+        query.find({
+          success: function(results){
+            for (var i = 0; i < results.length; i++) {
+              var object = results[i];
+              var photoURL = object.get("photo").url();
+              var html =  '<div class="artWrapper">' +
+                          '<div class="artInfo">' +
+                          '<h2>Area:</h2>' +
+                          object.get('area') +
+                          '<h2>Time Registered:</h2>' +
+                          object.get('createdAt') +
+                          '<h2>Details:</h2>' +
+                          object.get('comments') +
+                          '</div>' +
+                          '<div class="image" style="background-image: url('+photoURL+')""></div>' +
+                          '</div>';
+              $('#artModal').append(html);
+            }
+          },
+          error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
+        });
+
+        $('#artModal').css('display', 'block');
+      });
   }
 
 
--- After School/register.js
+++ After School/register.js
@@ -3,43 +3,77 @@ Parse.$ = jQuery;
 Parse.initialize('L6ShGalrKTMHYyEZY4c9sLqPWQIjWMTz08Hf9hdw',
 'W0CAhMkDQWEMWDIabWElRIq17xCEz1hv3iLTZWfd');
 
-$(function() {
+(function(){
 
   $(document).ready(init);
 
   function init() {
+    console.log('you are going');
     $('#submitDestination').on('click', newDestination);
   }
 
-
-//=============Form Submission
-
   function newDestination() {
-
-    console.log('you have started a new dest.');
+    console.log('inside function');
 
     var destination = new Parse.Object('Destination');
-    var name = $('#destinationName').val();
-    var address = $('#destinationAddress').val();
-    var programs = $('#desitinationPrograms').val();
+
+    var name = $('#name').val();
+    var address = $('#address').val();
+    var programs = $('#programs').val();
     var website = $('#website').val();
     var cost = $('#cost option:selected').text();
-
-    console.log(destination);
+    var type = $('#type option:selected').text();
 
     destination.set('name', name);
     destination.set('address', address);
     destination.set('programs', programs);
     destination.set('website', website);
     destination.set('cost', cost);
+    destination.set('type', type);
 
     destination.save(null, {
       success: function(destination){
         window.location.replace('home.html');
       },
-      error: function(tag, error){
+      error: function(error){
         alert('Sorry, there was an error saving your destination.');
       }
     });
   }
-});
+
+//   function init() {
+//     $('#submitDestination').on('click', newDestination);
+//   }
+//
+//
+// //=============Form Submission
+//
+//   function newDestination() {
+//
+//     console.log('you have started a new dest.');
+//
+//     var destination = new Parse.Object('Destination');
+//     var name = $('#destinationName').val();
+//     var address = $('#destinationAddress').val();
+//     var programs = $('#desitinationPrograms').val();
+//     var website = $('#website').val();
+//     var cost = $('#cost option:selected').text();
+//
+//     console.log(destination);
+//
+//     destination.set('name', name);
+//     destination.set('address', address);
+//     destination.set('programs', programs);
+//     destination.set('website', website);
+//     destination.set('cost', cost);
+//
+//     destination.save(null, {
+//       success: function(destination){
+//         window.location.replace('home.html');
+//       },
+//       error: function(destination, error){
+//         console.log('Sorry, there was an error saving your destination.');
+//       }
+//     });
+//   }
+})();
